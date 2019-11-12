b434c3cec38d502407fba2553643d7bedc788ee8
alisson.cavalcanti <alisson.cavalcanti@LAB-DEINFO-01.deinfo.lab33>
2019-11-12 15:57:34 -0300

    Pensamento a respeito...

--- src/ManipuladorArquivo.java
+++ src/ManipuladorArquivo.java
@@ -1,17 +1,23 @@
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 
 public class ManipuladorArquivo {
 
-	
+	//FileOutputStream out = null;
+//	ArrayList<Crime>crimes = new ArrayList<Crime>();
 	public static void leitor(String path) throws IOException {
         BufferedReader buffRead = new BufferedReader(new FileReader(path));
-        String linha = "";
+      //  out = new FileOutputStream("copiadeproverbiochines.txt");
+        
+        String linha = ",";
         while (true) {
             if (linha != null) {
+            	//Crime
                 System.out.println(linha);
  
             } else
@@ -20,4 +26,17 @@ public class ManipuladorArquivo {
         }
         buffRead.close();
     }
+	public static void mortesNov(String path) throws IOException
+	{
+	 
+		Crime crime;
+		
+		crime.setData(leitor(path));
+		
+		crimes.add(leitor(path));
+		leitor(path);
+		 
+	
+		
+	}
 }
